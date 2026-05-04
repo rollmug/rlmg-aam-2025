76741c730b19ae7b9144e923a43fb16669c795b3
Dave Kobrenski <dave@bbmdesigns.com>
2026-05-04 14:14:01 -0400

    removed output: "standalone" for next

--- rlmg-aam-app/next.config.mjs
+++ rlmg-aam-app/next.config.mjs
@@ -1,8 +1,8 @@
 // import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
